a431b6eaf10d2cc1c4ab83142add64d1b2388206
Maycon Jesus <contato@mayconjesus.dev>
2026-07-01 23:30:52 -0300

    feat: adicionar páginas de padrões e treinamento, e atualizar traduções

--- app/components/layouts/NavDrawer.vue
+++ app/components/layouts/NavDrawer.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
+import type { RouteLocationAsPathGeneric, RouteLocationAsRelativeGeneric } from 'vue-router'
+
 const { t } = useI18n()
 
 const layoutState = useLayoutStateStore()
 
-const navItems = computed(() => [
-  { title: t('nav.play'), icon: 'mdi-play', to: '/' },
-  { title: t('nav.settings'), icon: 'mdi-cog', to: '/settings' },
+const navItems = computed<{
+  title: string
+  icon: string
+  to: string | RouteLocationAsRelativeGeneric | RouteLocationAsPathGeneric | undefined
+}[]>(() => [
+  { title: t('nav.play'), icon: 'mdi-play', to: {name: 'index'} },
+  { title: t('nav.training'), icon: 'mdi-dumbbell', to: {name: 'training'} },
+  { title: t('nav.patterns'), icon: 'mdi-view-grid', to: {name: 'patterns'} },
+  { title: t('nav.settings'), icon: 'mdi-cog', to: {name: 'settings'} },
 ])
 </script>
 
 <template>
   <v-navigation-drawer v-model="layoutState.drawerOpen">
     <v-list nav>
-      <v-list-item v-for="item in navItems" :key="item.to" :to="item.to" :prepend-icon="item.icon"
+      <v-list-item v-for="item in navItems" :key="item.title" :to="item.to" :prepend-icon="item.icon"
         :title="item.title" />
     </v-list>
   </v-navigation-drawer>
