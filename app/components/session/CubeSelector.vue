088a11d31b314bafe14b089eceb8b1bd08c3c8ff
Maycon Jesus <contato@mayconjesus.dev>
2026-07-06 01:23:21 -0300

    fix: atualizar versão para 0.9.1 no package.json e definir tipo de Item no CubeSelector.vue

--- app/components/session/CubeSelector.vue
+++ app/components/session/CubeSelector.vue
@@ -22,6 +22,7 @@ v-model="model" :label="t('puzzle.label')" :items="items" hide-details item-titl
 </template>
 
 <script setup lang="ts">
+import type { Component } from 'vue';
 import { cubesDefinition } from '~~/lib/cube/cubesDefinition';
 
 const props = defineProps<{
@@ -32,8 +33,14 @@ const model = defineModel<string>()
 
 const { t } = useI18n()
 
-const items = computed(() => {
-    const all = Object.entries(cubesDefinition).map(([, cube]) => {
+type Item = {
+    value: string
+    title: string
+    icon?: Component
+}
+
+const items = computed<Item[]>(() => {
+    const all: Item[] = Object.entries(cubesDefinition).map(([, cube]) => {
         return {
             value: cube.id,
             title: t(`cube.${cube.id}`),
