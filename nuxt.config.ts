599293e232e40f5e3527789d7c06568e233fc5cc
Maycon Jesus <contato@mayconjesus.dev>
2026-07-07 18:05:57 -0300

    feat: adicionar ícones de favicon e apple-touch-icon no nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,6 +43,12 @@ export default defineNuxtConfig({
         { name: 'twitter:image:alt', content: 'Cube Trainer' },
       ],
       link: [
+        // Favicon
+        { rel: 'icon', href: '/favicon.ico', sizes: '32x32' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
         // PWA
         { rel: 'manifest', href: '/manifest.webmanifest' },
       ],
